0b90934cdfc600739f6e95c0e6c7afbb763c8aa8
giacomo <rota.giacomo.studente@itispaleocapa.it>
2025-12-23 14:14:06 +0100

    Struttura base e metodo aggiunta campo "miovalore"

---
+++ ElaborazioneCSV.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class ElaborazioneCSV {
+    private void aggiungiCampoCSV(String[] campo, File fileCSV) throws IOException {
+        File temp = new File("temp.csv");
+        FileWriter fileWriter = new FileWriter(temp);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileCSV));
+        for (int i=0; i<campo.length; i++){
+            String linea = bufferedReader.readLine();
+            printWriter.println(linea + "," + campo[i]);
+        }
+        printWriter.close();
+        bufferedReader.close();
+        Files.move(temp.toPath(), fileCSV.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public void aggiungiMioCampo(File file) throws IOException{
+        int numeroRecord = numeroDiRecord(file);
+        String[] recordMioValore = new String[numeroRecord+1];
+        recordMioValore[0] = "mio valore";
+        for (int i = 1; i<recordMioValore.length; i++){
+            recordMioValore[i] = Integer.toString((int)(Math.random()*11)+10);
+        }
+        aggiungiCampoCSV(recordMioValore, file);
+    }
+
+    private int numeroDiRecord(File file) throws IOException{
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int numeroRecord = 0;
+        while (bufferedReader.readLine() != null){
+            numeroRecord++;
+        }
+        bufferedReader.close();
+        return numeroRecord-1;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        File file = new File("rota.csv");
+        ElaborazioneCSV classeLavoro = new ElaborazioneCSV();
+        classeLavoro.aggiungiMioCampo(file);
+    }
+}
