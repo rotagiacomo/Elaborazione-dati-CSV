abb448d030a8209aafdf0a9ec7849b8d3d79ef13
giacomo <rota.giacomo.studente@itispaleocapa.it>
2026-01-06 11:14:10 +0100

    Implementazione menu

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         File file = new File("rota.csv");
-        ElaborazioneCSV classeLavoro = new ElaborazioneCSV();
-        classeLavoro.aggiungiRecord(recordCasuale(classeLavoro.numeroDiCampi(file)), file);
-        classeLavoro.aggiungiMioCampo(file);
-        classeLavoro.aggiungiCampoCancellazioneLogica(file);
-        System.out.println(classeLavoro.recodDiCampo("County", 2, file));
-        //String[] campi = classeLavoro.mostraCampi(new String[]{"County", "City"}, file);
-        //stampaArrString(campi);
-
-        classeLavoro.dimensioneFissa(file);
+        avviaMenu(file);
     }
 
     public static String intToString(int[] interi){
@@ -38,5 +31,61 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+    }
+
+    public static void avviaMenu(File file) throws IOException{
+        ElaborazioneCSV elaborazioneCSV = new ElaborazioneCSV();
+        Scanner scanner = new Scanner(System.in);
+        int opzione = 0;
+        do{
+           System.out.println("1) aggiungere campo mio valore");
+           System.out.println("2) aggiungere campo cancellazione logica");
+           System.out.println("3) contare il numero dei campi");
+           System.out.println("4) lunghezza di ogni campo");
+           System.out.println("5) dimensione fissa");
+           System.out.println("6) aggiungere record in coda");
+           System.out.println("7) mostra campi");
+           System.out.println("8) ricerca record ");
+           System.out.println("9) modifica record");
+           System.out.println("10) cancella logicamente record");
+           System.out.println("0) uscire");
+           opzione = Integer.parseInt(scanner.nextLine());
+           switch (opzione){
+               case 1:
+                   elaborazioneCSV.aggiungiMioCampo(file);
+                   break;
+               case 2:
+                   elaborazioneCSV.aggiungiCampoCancellazioneLogica(file);
+                   break;
+               case 3:
+                   System.out.println("Numero di campi: " + elaborazioneCSV.numeroDiCampi(file));
+                   break;
+               case 4:
+                   //Da implementare
+                   break;
+               case 5:
+                   elaborazioneCSV.dimensioneFissa(file);
+                   break;
+               case 6:
+                   elaborazioneCSV.aggiungiRecord(recordCasuale(elaborazioneCSV.numeroDiCampi(file)), file);
+                   break;
+               case 7:
+                   //Da implementare
+                   break;
+               case 8:
+                   System.out.println("Di quale campo?");
+                   String campo = scanner.nextLine();
+                   System.out.println("A quale riga?");
+                   int riga = Integer.parseInt(scanner.nextLine());
+                   System.out.println("Contenuto: " + elaborazioneCSV.recodDiCampo(campo, riga, file));
+                   break;
+               case 9:
+                   //Da implementare
+                   break;
+               case 10:
+                   //Da implementare
+                   break;
+           }
+        }while (opzione !=0);
     }
 }
